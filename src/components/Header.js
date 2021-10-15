ae3265dac1a727125871bb4b9471e60285772f61
John Schaub <schaubjs@gmail.com>
2021-10-14 18:13:11 -0600

    header, footer, addtask components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,36 @@
 import PropTypes from 'prop-types';
-import Button from './Button'
+import { useLocation } from 'react-router-dom';
+import Button from './Button';
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log('click')
+const Header = ({ title, onAdd, showAdd }) => {
+	const location = useLocation();
 
-  }
-  
-  return (
+	return (
 		<header className='header'>
 			<h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick} />
-			
+			{location.pathname === '/' && (
+				<Button
+					color={showAdd ? 'red' : 'green'}
+					text={showAdd ? 'Close' : 'Add'}
+					onClick={onAdd}
+				/>
+			)}
 		</header>
-  );
-}
-Header.defaultProps = {
-  title: 'Task Tracker'
-}
+	);
+};
 
+Header.defaultProps = {
+	title: 'Task Tracker',
+};
 
 Header.propTypes = {
-  title: PropTypes.string.isRequired, 
-}
+	title: PropTypes.string.isRequired,
+};
 
-export default Header
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black',
+// }
+
+export default Header;
